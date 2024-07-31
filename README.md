469bdf716da90fe370c8034877627b7fe957094a
barami62 <wodbs7758@naver.com>
2024-08-01 01:52:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MySelectShop
---
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+//@EnableJpaAuditing
+@EnableScheduling
+public class MyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopApplication.class, args);
+    }
+}
